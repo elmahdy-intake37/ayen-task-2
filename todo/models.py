dc50b014e8348f7b5fdb4d236eafbd120719459f
elmahdy <elmahdy30@gmail.com>
2020-09-09 13:03:58 +0200

    ayen task

---
+++ todo/models.py
@@ -0,0 +1,29 @@
+import abc
+
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class Task(models.Model):
+    """
+    A Stateful Task that contains a title and a description.
+    and can be linked to other tasks in a on-to relation,
+    """
+    NEW = 0
+    IN_PROGRESS = 1
+    DONE = 2
+
+    STATES = [
+        (NEW, _('New')),
+        (IN_PROGRESS, _('In Progress')),
+        (DONE, _('Done'))
+    ]
+
+    title = models.CharField(_('Title'), max_length=100)
+    description = models.TextField(_('Description'))
+    created_at = models.DateTimeField(auto_now_add=True)
+    state = models.SmallIntegerField(choices=STATES, default=NEW)
+    children = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.STATES[self.state][1]}"
